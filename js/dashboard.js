4904c64ea7429812c0fcf7e7ff99a26ff2be3366
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2022-03-26 02:12:26 -0400

    dashboard content

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,16 +2,24 @@ const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
 const categories = {
-  "animatronics": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1154055729&amp;single=true&amp;widget=true&amp;headers=false`,
+  "animatronics": ``,
   "laberinto": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false`,
-  "lego sumo": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`,
+  "lego sumo": ``,
   "micro sumo": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`,
   "mini sumo": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`,
   "robofut": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`,
   "seguidor de lineas abierto": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`,
-  "Seguidor de lineas lego kit": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`,
-  "reto lego": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`
+  "Seguidor de lineas lego kit": ``,
+  "reto lego": ``
 };
 
-document.getElementById("Title").innerText =params.category.charAt(0).toUpperCase() + params.category.slice(1);
-document.getElementById("sheets").src=categories[params.category]
+document.title = params.category.charAt(0).toUpperCase() + params.category.slice(1);
+
+if (categories[params.category] != ''){
+  document.getElementById("Title").innerText =params.category.charAt(0).toUpperCase() + params.category.slice(1);
+  document.getElementById("sheets").src=categories[params.category];
+}
+else{
+  document.getElementById("sheets").remove();
+  $("#Title").after("<div id='error-message' class='container-fluid'><h2>Contenido no disponible<h2></div>")
+}
