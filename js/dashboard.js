82da954358662ffc6eaa9a75ca9029521d1519f5
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2022-03-26 07:47:07 -0400

    main

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,12 +3,12 @@ const params = Object.fromEntries(urlSearchParams.entries());
 
 const categories = {
   "animatronics": ``,
-  "laberinto": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false`,
+  "laberinto": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSJaan2QGIIcXDgNNeySTM6fuyM0HG2D0_cPyW39_nNa818kmJ5IzODgZCZp-ETfipUKx-L5J1iaJVW/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false`,
   "lego sumo": ``,
-  "micro sumo": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`,
-  "mini sumo": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`,
-  "robofut": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`,
-  "seguidor de lineas abierto": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSmrpRwiI_jBK7ngUEJ-qVqCL4uZGeEqtQkYW9F_g-_4jC0V9-IkSBzjav4LcVKA95KhdnBjKnQqB8Y/pubhtml?gid=1743632872&amp;single=true&amp;widget=true&amp;headers=false`,
+  "micro sumo": `https://docs.google.com/spreadsheets/d/e/2PACX-1vSW3RA8whq_qXI6DbDIakwYQYaivEt0IZ4ILGykDiOqJ1X2M_6iHe7mVUf5SX5vuX9W0Ofv5kzKQ80G/pubhtml?gid=168779283&amp;single=true&amp;widget=true&amp;headers=false`,
+  "mini sumo": `https://docs.google.com/spreadsheets/d/e/2PACX-1vTI0O6iSlOIrNRY2NuXTfI75nvU92X5vbLsaTYQ3nS5eiYtjzU52xrsML3R_Hjnc_cqDyW_vsfPRX4f/pubhtml?gid=739142431&amp;single=true&amp;widget=true&amp;headers=false`,
+  "robofut": `https://docs.google.com/spreadsheets/d/e/2PACX-1vTFIf7ba5TmZrb_8xNoLYiQq03bbUZY5k9VkoLJJrHiQNzFCf2cDW7guf6_OWmgTnT4qD__LtxvnmN2/pubhtml?gid=698525493&amp;single=true&amp;widget=true&amp;headers=false`,
+  "seguidor de lineas abierto": `https://docs.google.com/spreadsheets/d/e/2PACX-1vRWy3Ad4YZfUJcvnGE-M6SbBxo9CVbBNbkbWC0sgtbSMhVLLtWbKIqEHTMuqpVDPQowqc2YU293n8K4/pubhtml?gid=407408284&amp;single=true&amp;widget=true&amp;headers=false`,
   "Seguidor de lineas lego kit": ``,
   "reto lego": ``
 };
